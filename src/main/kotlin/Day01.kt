3e8c05c14e4265591fb7e485adcf14db9a6f9423
Anita Kosman <anita.kosman@gxsoftware.com>
2025-12-01 20:32:26 +0100

    Day 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun main() {
     val input = getInputLines("day1")
 
@@ -6,9 +8,30 @@ fun main() {
 }
 
 private fun part1(input: List<String>): Int {
-    return 0
+    var dial = 50
+    var code = 0
+    input.forEach { s ->
+        dial = if (s.startsWith('L')) (dial - s.substring(1).toInt()).mod(100) else (dial + s.substring(1).toInt()).mod(100)
+        if (dial == 0) {
+            code++
+        }
+    }
+    return code
 }
 
 private fun part2(input: List<String>): Int {
-    return 0
+    var dial = 50
+    var code = 0
+    input.forEach { s ->
+        val oldDial = dial
+        dial = if (s.startsWith('L')) dial - s.substring(1).toInt() else dial + s.substring(1).toInt()
+        if (dial < 0 && oldDial == 0) {
+            dial += 100
+        }
+        if (dial !in 1..99) {
+            code += if (dial <= 0) (abs(dial) / 100) + 1 else dial / 100
+        }
+        dial = dial.mod(100)
+    }
+    return code
 }

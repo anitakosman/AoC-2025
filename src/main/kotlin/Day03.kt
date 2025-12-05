673bb502ebfc29ba8590f3682b61f1413b836226
Anita Kosman <anitakosman@hotmail.com>
2025-12-05 20:41:13 +0100

    Day 3

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,14 +1,27 @@
+import java.math.BigInteger
+import kotlin.io.println
+
 fun main() {
-    val input = getInputLines("day3")
+    val input = getInputLines("day3").map { line -> line.map { it.toString().toInt() } }
 
     println(part1(input))
     println(part2(input))
 }
 
-private fun part1(input: List<String>): Int {
-    return 0
+private fun part1(input: List<List<Int>>): BigInteger {
+    return input.sumOf { maxJoltage(it, 2) }
 }
 
-private fun part2(input: List<String>): Int {
-    return 0
+private fun part2(input: List<List<Int>>): BigInteger {
+    return input.sumOf { maxJoltage(it, 12) }
+}
+
+private fun maxJoltage(batteries: List<Int>, numberOfBatteries: Int): BigInteger {
+    val x = batteries.dropLast(numberOfBatteries - 1).max()
+    if (numberOfBatteries == 1) {
+        return x.toBigInteger()
+    } else {
+        val y = maxJoltage(batteries.drop(batteries.indexOf(x) + 1), numberOfBatteries - 1)
+        return BigInteger.valueOf(10).pow(numberOfBatteries - 1) * x.toBigInteger() + y
+    }
 }
